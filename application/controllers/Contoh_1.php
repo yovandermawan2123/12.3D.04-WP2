06d6f938dc8e4ac090f675150cf164b7db4692d4
yovandermawan2123 <yovanwota@gmail.com>
2022-09-13 20:49:11 +0700

    Menambahkan Controller

---
+++ application/controllers/Contoh_1.php
@@ -0,0 +1,13 @@
+<?php
+
+class Contoh_1 extends CI_Controller {
+
+
+	public function index()
+	{
+        echo "Biodata";
+        echo"<br>";
+        echo "Nama Saya Yovan Dermawan, Bapak bisa panggil saya <strong>Yovan</strong> saya dari program studi Sistem Informasi D3";
+		// $this->load->view('welcome_message');
+	}
+}
